7e19d5a48c676fa18b9f935191c333d4e6d4d760
HuntScript <huntscript54@gmail.com>
2024-01-05 18:46:13 -0500

    Create lst_elements_strings.py

---
+++ lists/part1/lst_elements_strings.py
@@ -0,0 +1,13 @@
+# List of strings
+
+# Creating a list of strings
+fav = ["Python", "C++", "C#"]
+names = ["Ken","Hunter"]
+
+# print favorites and names
+print(fav) 
+print(names)
+# print the length of 'fav' 
+print("'fav' has", len(fav), "favorites.") 
+# print the length of 'names' 
+print("'fav' has", len(names), "names.")
