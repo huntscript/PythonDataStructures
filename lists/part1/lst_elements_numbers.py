8e94dce0a91d533ed63446b8c3684fd9f123d87f
HuntScript <huntscript54@gmail.com>
2024-01-05 18:48:18 -0500

    Create lst_elements_numbers.py

---
+++ lists/part1/lst_elements_numbers.py
@@ -0,0 +1,13 @@
+# List of numbers
+
+# Creating lists containing numbers
+scores = [10.9, 15.8, 20.4]
+ages = [19, 17, 15, 18]
+
+# print the scores and ages
+print(scores) 
+print(ages)
+# print the length of 'scores' 
+print("'scores' contains", len(scores), "scores.") 
+# print the length of 'ages' 
+print("'ages' contains", len(ages), "ages.")
