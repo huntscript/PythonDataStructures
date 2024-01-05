f2c990acfae6545231d2c65976d070ad13445f68
HuntScript <huntscript54@gmail.com>
2024-01-05 18:50:51 -0500

    Create lst_elements_mixed.py

---
+++ lists/part1/lst_elements_mixed.py
@@ -0,0 +1,15 @@
+# List of elements with mixed data types
+
+# Initializing variables
+connect = "connection"
+rate = 100
+speed = 95.89
+opened = True
+# Creating the list of elements
+evaluation = [connect, rate, speed, opened]
+
+# print 'evaluation'
+print(evaluation)
+# get the length of 'evaluation' 
+ev_length = len(evaluation) # initialization
+print("'evaluation' has", ev_length , "elements.") 
