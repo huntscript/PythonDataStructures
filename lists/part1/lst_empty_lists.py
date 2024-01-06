7d94c8e6aa73d01849139f99484bf2dac0d9443a
HuntScript <huntscript54@gmail.com>
2024-01-06 08:59:34 -0500

    Update lst_empty_lists.py

--- lists/part1/lst_empty_lists.py
+++ lists/part1/lst_empty_lists.py
@@ -6,7 +6,7 @@ a_list = []
 another list = list()
 
 print(a_list) # print the first list
-print(len(a_list)) # number of elements
+print("number of elements", len(a_list)) # number of elements
 print() # new line character
 print(another_list) # print the second list
-print(len(another_list)) # number of elements
+print("number of elements", len(another_list)) # number of elements
